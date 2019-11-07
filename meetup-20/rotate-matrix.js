ef141310e76d9c6dc900e9744f4d5328f3b73114
Matt Weidner <mweidner44@gmail.com>
2019-11-07 20:32:23 +0900

    Co-authored-by: Matthew Cook <mc255v@gmail.com>
    solved with nested for loops!

---
+++ meetup-20/rotate-matrix.js
@@ -0,0 +1,27 @@
+const input = [
+    [ 1, 2, 3, 4, 5 ],
+    [ 6, 7, 8, 9, 10 ],
+    [ 'A', 'B', 'C', 'D', 'E' ],
+    [ 'F', 'G', 'H', 'I', 'J' ]
+];
+
+const output = [
+    [ 'F', 'A', 6, 1 ],
+    [ 'G', 'B', 7, 2 ],
+    [ 'H', 'C', 8, 3 ],
+    [ 'I', 'D', 9, 4 ],
+    [ 'J', 'E', 10, 5 ]
+];
+
+const rotater = (matrix) => {
+    const answer = []
+    for (let i = 0; i < matrix[i].length; i++) {
+        answer.push([])
+        for (let j = matrix.length - 1; j >= 0; j--) {
+            answer[i].push(matrix[j][i]);
+        }
+    }
+    return answer
+}
+
+rotater(input)
